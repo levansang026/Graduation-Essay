71c48173f759d53c63c1f740ba81d3032faea152
bill <levansang026@gmail.com>
2016-12-06 20:19:39 +0700

    add news

---
+++ SampleProjects/VuforiaSample/VuforiaSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VuforiaSample
+//
+//  Created by VAN DAO on 12/6/16.
+//  Copyright © 2016 VAN DAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "VuforiaSamplesAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VuforiaSamplesAppDelegate class]));
+    }
+}
